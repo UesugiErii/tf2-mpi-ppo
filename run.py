0e210de0f2f3b07e6087a468991ec0f7bfd23ed2
UesugiErii <zhangxu.me@gmail.com>
2020-09-14 12:29:04 +0800

    prepare all data

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-# Integrated neural network
+# prepare all data
 # mpirun -n 9 --oversubscribe python3 run.py
 
 from mpi4py import MPI
@@ -27,9 +27,43 @@ import gym
 env = gym.make(env_name)
 a_num = env.action_space.n  # env.action_space
 del env
+gamma = 0.99  # discount reward
 
 # brain
 if rank == 0:
+    def calc_realv_and_adv_GAE(v, r, done):
+        length = r.shape[0]
+        num = r.shape[1]
+
+        adv = np.zeros((length + 1, num), dtype=np.float32)
+
+        for t in range(length - 1, -1, -1):
+            delta = r[t, :] + v[t + 1, :] * gamma * (1 - done[t, :]) - v[t, :]
+            adv[t, :] = delta + gamma * 0.95 * adv[t + 1, :] * (1 - done[t, :])
+
+        adv = adv[:-1, :]
+
+        realv = adv + v[:-1, :]
+
+        return realv, adv
+
+
+    def calc_realv_and_adv(v, r, done):
+        length = r.shape[0]
+        num = r.shape[1]
+
+        realv = np.zeros((length + 1, num), dtype=np.float32)
+        adv = np.zeros((length, num), dtype=np.float32)
+
+        realv[-1, :] = v[-1, :] * (1 - done[-1, :])
+
+        for t in range(length - 1, -1, -1):
+            realv[t, :] = realv[t + 1, :] * gamma * (1 - done[t, :]) + r[t, :]
+            adv[t, :] = realv[t, :] - v[t, :]
+
+        return realv[:-1, :], adv  # end_v dont need
+
+
     import tensorflow as tf
     from tensorflow.python.keras import Model
     from tensorflow.python.keras.layers import Dense, Conv2D, Flatten
@@ -63,7 +97,7 @@ if rank == 0:
 
     model = CNNModel()
 
-    total_state = np.empty((epochs + 1, size - 1, 84, 84, k), dtype=np.float32)
+    total_state = np.empty((epochs, size - 1, 84, 84, k), dtype=np.float32)
     total_v = np.empty((epochs + 1, size - 1), dtype=np.float32)
     total_a = np.empty((epochs, size - 1), dtype=np.float32)
     total_r = np.zeros((epochs, size - 1), dtype=np.float32)
@@ -81,20 +115,27 @@ if rank == 0:
 
     ap, v = model(current_state)
     ap = ap.numpy()
-    # v = v.numpy()
-
-    # print(ap[0])
-    assert ap.shape == (size - 1, a_num)
-    assert v.shape == (size - 1, 1)
+    v = v.numpy()
+    v.resize((size - 1,))
 
     # scattering action
-    a = [0] + [np.random.choice(range(a_num), p=ap[i]) for i in range(size - 1)]
+    a = [np.random.choice(range(a_num), p=ap[i]) for i in range(size - 1)]
+    total_a[0, :] = a
+    a = [0] + a
     a = comm.scatter(a, root=0)
 
     # recv other information
     r = comm.gather(r, root=0)
     done = comm.gather(done, root=0)
     info = comm.gather(info, root=0)
+    del r[0]
+    del done[0]
+    del info[0]
+
+    total_v[0, :] = v
+    total_r[0, :] = np.array(r, dtype=np.float32)
+    total_done[0, :] = np.array(done, dtype=np.float32)
+    total_old_ap[0, :] = np.array(ap, dtype=np.float32)
 
     # reset other information
     r = 0
@@ -103,26 +144,36 @@ if rank == 0:
 
     # 255 loop ( Don't write exit conditions temporarily )
     while 1:
-        for epoch in range(epochs - 1):
+        for epoch in range(1, epochs):
             # recv state
             comm.Gather(send_state_buf, recv_state_buf, root=0)
 
             current_state = recv_state_buf[1:size, :, :, :]
-            total_state[epoch + 1, :, :, :, :] = current_state
+            total_state[epoch, :, :, :, :] = current_state
 
             ap, v = model(current_state)
             ap = ap.numpy()
-            assert ap.shape == (size - 1, a_num)
-            assert v.shape == (size - 1, 1)
+            v = v.numpy()
+            v.resize((size - 1,))
 
             # scattering action
-            a = [0] + [np.random.choice(range(a_num), p=ap[i]) for i in range(size - 1)]
+            a = [np.random.choice(range(a_num), p=ap[i]) for i in range(size - 1)]
+            total_a[epoch, :] = a
+            a = [0] + a
             a = comm.scatter(a, root=0)
 
             # recv other information
             r = comm.gather(r, root=0)
             done = comm.gather(done, root=0)
             info = comm.gather(info, root=0)
+            del r[0]
+            del done[0]
+            del info[0]
+
+            total_v[epoch, :] = v
+            total_r[epoch, :] = np.array(r, dtype=np.float32)
+            total_done[epoch, :] = np.array(done, dtype=np.float32)
+            total_old_ap[epoch, :] = np.array(ap, dtype=np.float32)
 
             # reset other information
             r = 0
@@ -132,30 +183,48 @@ if rank == 0:
         comm.Gather(send_state_buf, recv_state_buf, root=0)
 
         current_state = recv_state_buf[1:size, :, :, :]
-        total_state[epoch + 1, :, :, :, :] = current_state
 
         ap, v = model(current_state)  # dont need ap
-        assert ap.shape == (size - 1, a_num)
-        assert v.shape == (size - 1, 1)
+        v = v.numpy()
+        v.resize((size - 1,))
+        total_v[-1, :] = v
 
+        total_real_v, total_adv = calc_realv_and_adv_GAE(total_v, total_r, total_done)
+        total_state.resize((epochs * (size - 1), 84, 84, k))
+        total_a.resize((epochs * (size - 1),))
+        total_old_ap.resize((epochs * (size - 1), a_num))
+        total_adv.resize((epochs * (size - 1),))
+        total_real_v.reshape((epochs * (size - 1),))
         # TODO: use all information update network
+        total_state.resize((epochs, size - 1, 84, 84, k))
+        total_a.resize((epochs, size - 1))
+        total_old_ap.resize((epochs, size - 1, a_num))
+
 
         # move last one to first one
-        total_state[0, :, :, :, :] = current_state
-
         ap, v = model(current_state)
         ap = ap.numpy()
-        assert ap.shape == (size - 1, a_num)
-        assert v.shape == (size - 1, 1)
+        v = v.numpy()
+        v.resize((size - 1,))
 
         # scattering action
-        a = [0] + [np.random.choice(range(a_num), p=ap[i]) for i in range(size - 1)]
+        a = [np.random.choice(range(a_num), p=ap[i]) for i in range(size - 1)]
+        total_a[0, :] = a
+        a = [0] + a
         a = comm.scatter(a, root=0)
 
         # recv other information
         r = comm.gather(r, root=0)
         done = comm.gather(done, root=0)
         info = comm.gather(info, root=0)
+        del r[0]
+        del done[0]
+        del info[0]
+
+        total_v[0, :] = v
+        total_r[0, :] = np.array(r, dtype=np.float32)
+        total_done[0, :] = np.array(done, dtype=np.float32)
+        total_old_ap[0, :] = np.array(ap, dtype=np.float32)
 
         # reset other information
         r = 0
